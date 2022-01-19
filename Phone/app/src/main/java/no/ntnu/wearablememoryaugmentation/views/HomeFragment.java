6452ebf38d7555fb603b12fb72f3cf305705f7ed
Adrian <adrianrflatner@gmail.com>
2022-01-19 12:24:58 +0100

    Adding fragments for card and navigation

--- Phone/app/src/main/java/no/ntnu/wearablememoryaugmentation/views/HomeFragment.java
+++ Phone/app/src/main/java/no/ntnu/wearablememoryaugmentation/views/HomeFragment.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
@@ -58,14 +59,22 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         loggedInUserTextView = view.findViewById(R.id.fragment_loggedin_loggedInUser);
-        logOutButton = view.findViewById(R.id.fragment_loggedin_logOut);
+        /*logOutButton = view.findViewById(R.id.fragment_loggedin_logOut);
 
         logOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 homeViewModel.logOut();
             }
-        });
+        });*/
+
+        Fragment cardFragment = new CardFragment();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.card_placeholder, cardFragment).commit();
+
+        Fragment navigationFragment = new NavigationFragment();
+        FragmentTransaction transaction2 = getChildFragmentManager().beginTransaction();
+        transaction2.add(R.id.navigation_placeholder, navigationFragment).commit();
 
         return view;
     }
