3d49a74a00109cb76b68d50183ebfbdb77f584d5
Adrian <adrianrflatner@gmail.com>
2022-01-21 17:40:18 +0100

    Flip card

--- Phone/app/src/main/java/no/ntnu/wearablememoryaugmentation/views/CardFragment.java
+++ Phone/app/src/main/java/no/ntnu/wearablememoryaugmentation/views/CardFragment.java
@@ -26,11 +26,17 @@ public class CardFragment extends Fragment {
 
     private CardViewModel cardViewModel;
     private TextView cueText;
+    private TextView cueInfo;
+    private TextView currentInfo;
+    private TextView cueTextSmall;
     private ArrayList<Cue> cueArrayList;
+    private Boolean isFlipped;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        isFlipped = false;
         cardViewModel = new ViewModelProvider(this).get(CardViewModel.class);
         cardViewModel.getCueListMutableLiveData().observe(this, new Observer<ArrayList<Cue>>() {
             @Override
@@ -38,9 +44,9 @@ public class CardFragment extends Fragment {
                 int i = 0;
                 if (cues != null){
                     cueText.setText(cues.get(i).cue);
+                    cueInfo.setText(cues.get(i).info);
                     cueArrayList = cues;
                 }
-
             }
         });
     }
@@ -50,15 +56,23 @@ public class CardFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_card, container, false);
 
+        currentInfo = view.findViewById(R.id.current_info);
+        cueTextSmall = view.findViewById(R.id.cue_text_small);
         cueText = view.findViewById(R.id.cue_text);
+        cueInfo = view.findViewById(R.id.cue_info);
+        cueInfo.setVisibility(TextView.INVISIBLE);
 
-       /* ConstraintLayout cardView = view.findViewById(R.id.card_view);
+        ConstraintLayout cardView = view.findViewById(R.id.card_view);
         cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                isFlipped = !isFlipped;
+                cueText.setVisibility(isFlipped ? TextView.INVISIBLE : TextView.VISIBLE);
+                cueInfo.setVisibility(!isFlipped ? TextView.INVISIBLE : TextView.VISIBLE);
+                currentInfo.setText(isFlipped ? "Information" : "Current cue");
+                cueTextSmall.setText(isFlipped ? cueText.getText() : "");
             }
-        });*/
+        });
 
         return view;
     }
